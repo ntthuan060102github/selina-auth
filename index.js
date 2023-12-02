fff9ed17a02e5d22ff5caad4fd76e51c761fdfdf
Nguyen Trong Thuan <87819498+ntthuan060102github@users.noreply.github.com>
2023-12-02 22:43:52 +0700

    health (#1) (#2)
    * readme
    
    * Update README.md
    
    * health
    
    ---------
    
    Co-authored-by: 20120578 <pqthai2502@gmail.com>

--- index.js
+++ index.js
@@ -41,6 +41,10 @@ app.get("/", (req, res) => {
     res.send(`Selina - Authorization Service (${APP_ENV})`)
 })
 
+app.get("/health", (req, res) => {
+    res.json({})
+})
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
